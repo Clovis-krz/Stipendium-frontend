e409e3cdd7f6b56b127cfce649c299b9ab7601ff
Clovis Krzyzanowski <clovis.krzyzanowski@epita.fr>
2022-03-21 22:22:04 +0100

    update transaction every 60 seconds

--- src/components/TransactionsList.vue
+++ src/components/TransactionsList.vue
@@ -6,18 +6,26 @@
             <table class="table table-striped table-bordered">
                 <thead>
                     <tr>
-                        <th>TIME</th>
+                        <th>TIME (UTC)</th>
                         <th>TRANSACTION NB</th>
                         <th>AMOUNT</th>
                         <th>STATUS</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="transaction in this.data.transactions" :key="transaction.time">
-                        <td>{{transaction.time}}</td>
+                    <tr v-for="transaction in this.data.transactions.slice().reverse()" :key="transaction.transaction_nb">
+                        <td>{{timestamp_to_time(transaction.time)}}</td>
                         <td>{{transaction.transaction_nb}}</td>
                         <td>{{transaction.amount_total}}</td>
-                        <td>{{transaction.status}}</td>
+                        <status-pending v-if="transaction.status == 'pending'">
+                            <td class="bg-warning">{{transaction.status}}</td>
+                        </status-pending>
+                        <status-failed v-if="transaction.status == 'failed'">
+                            <td class="bg-danger">{{transaction.status}}</td>
+                        </status-failed>
+                        <status-paid v-if="transaction.status == 'paid'">
+                            <td class="bg-success">{{transaction.status}}</td>
+                        </status-paid>
                     </tr>
                 </tbody>
             </table>
@@ -51,11 +59,25 @@ export default {
             .then(data => {
                 this.data = data;
             })
+      },
+      timestamp_to_time(s){
+          return new Date(s * 1000).toISOString().slice(0, 19).replace('T', ' ');
       }
 
   },
   mounted: function () {
-    }
+      window.setInterval(() => {
+        console.log(this.token);
+        fetch("http://backend.sc2aips9849.universe.wf/api/transactions?token="+this.token)
+            .then(response => response.json())
+            .then(data => {
+                this.data = data;
+            })
+    }, 60000)
+},
+
 }
+
+
 
 </script>
